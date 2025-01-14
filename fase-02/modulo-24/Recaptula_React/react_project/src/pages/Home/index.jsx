bceae887b7fa2c666c683061f9f01cf48cc3b726
Anderson Feltes <andermateus8@gmail.com>
2025-01-14 00:35:50 -0300

    Adicionando svg no React apartir de tags svg e img

--- fase-02/modulo-24/Recaptula_React/react_project/src/pages/Home/index.jsx
+++ fase-02/modulo-24/Recaptula_React/react_project/src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import reactLogo from '../../assets/react.svg'
+
 function Title({children, className}){
     return <h1 className = {className}> {children} </h1>
 }
@@ -8,7 +10,14 @@ export function Home(){
             <Title className = "React">
                 Ola novamente React
             </Title>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+            </svg>
+
+            <img src={reactLogo}/>
         </div>
+
     )
     
 }
