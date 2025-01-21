a20a0d4856ddef4170e2e1b09934c9dd5d18cccb
Anderson Feltes <andermateus8@gmail.com>
2025-01-20 23:03:16 -0300

    revendo os hooks do rect, useState e useEffect

--- fase-02/modulo-25/React/src/components/contador.jsx
+++ fase-02/modulo-25/React/src/components/contador.jsx
@@ -3,8 +3,11 @@ import { useState,useEffect } from "react"
 function Counter(){
     const [total, setTotal] = useState(0)
 
-    const add  = () => setTotal(total + 1) 
-    const remove = () => setTotal (total - 1)
+    // é preciso passar o valor com o prevStat para a função receber o ultimo estado em que se encontra o componente
+    // para alterar um estado que depende de outro é preciso passar uma função callback nos parametros retornando o valor 
+    // useState é assíncrono
+    const add  = () => setTotal(prevState => prevState + 1) 
+    const remove = () => setTotal (prevState => prevState - 1)
 
     useEffect( () => {
         console.log("Contador criado")
